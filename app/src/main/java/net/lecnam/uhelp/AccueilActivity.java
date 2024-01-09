432c186c4ff1895c3a80aedb07cfcced07fdd034
Lilierd 42 <lilierdyt@gmail.com>
2024-01-09 12:16:49 +0100

    Accueil complete

--- app/src/main/java/net/lecnam/uhelp/AccueilActivity.java
+++ app/src/main/java/net/lecnam/uhelp/AccueilActivity.java
@@ -1,10 +1,15 @@
 package net.lecnam.uhelp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.LayoutAnimationController;
 import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,12 +20,22 @@ public class AccueilActivity extends AppCompatActivity {
 
     private Button retour;
 
+    private GridLayout mesDemandes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.accueil);
 
         retour = (Button) findViewById(R.id.retour);
+        mesDemandes = (GridLayout) findViewById(R.id.mesdemandes);
+        for(int i = 0; i < 4; i++){
+            TextView t = new TextView(this);
+            t.setText("patate"+i);
+            t.setTextColor(Color.WHITE);
+            mesDemandes.addView(t);
+
+        }
 
         retour.setOnClickListener(v -> retourLanding());
     }
